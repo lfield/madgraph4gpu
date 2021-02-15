02dd3e3e5580d00989dd29d6b63e45f037a2bf68
Laurence <laurence.field@cern.ch>
2021-02-15 11:13:02 +0100

    Adding compare branch for ep2_sycl_ee_mumu

--- epoch2/cuda/ee_mumu/src/HelAmps_sm.h
+++ epoch2/cuda/ee_mumu/src/HelAmps_sm.h
@@ -9,7 +9,10 @@
 #ifndef HelAmps_sm_H
 #define HelAmps_sm_H
 
-#include <cmath> 
+#ifdef SYCL_LANGUAGE_VERSION
+#include <CL/sycl.hpp>
+#endif
+#include <cmath>
 #include "mgOnGpuConfig.h"
 #include "mgOnGpuTypes.h"
 
@@ -85,28 +88,28 @@ namespace MG5_sm
 // 
 // 
 
-__device__ void FFV1_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
+void FFV1_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
     const cxtype COUP, cxtype * vertex);
 
-__device__ void FFV1P0_3(const cxtype F1[], const cxtype F2[], const cxtype
+void FFV1P0_3(const cxtype F1[], const cxtype F2[], const cxtype
     COUP, const fptype M3, const fptype W3, cxtype V3[]);
 
-__device__ void FFV2_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
+void FFV2_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
     const cxtype COUP, cxtype * vertex);
 
-__device__ void FFV2_3(const cxtype F1[], const cxtype F2[], const cxtype COUP,
+void FFV2_3(const cxtype F1[], const cxtype F2[], const cxtype COUP,
     const fptype M3, const fptype W3, cxtype V3[]);
 
-__device__ void FFV4_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
+void FFV4_0(const cxtype F1[], const cxtype F2[], const cxtype V3[],
     const cxtype COUP, cxtype * vertex);
 
-__device__ void FFV4_3(const cxtype F1[], const cxtype F2[], const cxtype COUP,
+void FFV4_3(const cxtype F1[], const cxtype F2[], const cxtype COUP,
     const fptype M3, const fptype W3, cxtype V3[]);
 
-__device__ void FFV2_4_0(const cxtype F1[], const cxtype F2[], const cxtype
+void FFV2_4_0(const cxtype F1[], const cxtype F2[], const cxtype
     V3[], const cxtype COUP1, const cxtype COUP2, cxtype * vertex);
 
-__device__ void FFV2_4_3(const cxtype F1[], const cxtype F2[], const cxtype
+void FFV2_4_3(const cxtype F1[], const cxtype F2[], const cxtype
     COUP1, const cxtype COUP2, const fptype M3, const fptype W3, cxtype V3[]);
 
 }  // end namespace MG5_sm
